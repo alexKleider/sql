8291e8e119e1f82a2c60050a4f71ee3301bc7f62
Alex Kleider <alexkleider@protonmail.com>
2023-02-06 18:06:22 -0800

    querys 1 and 1a work fine; inner join >> wrong!

--- query1a.py
+++ query1a.py
@@ -3,24 +3,48 @@
 # File: query1a.py
 
 import sqlite3
-import add_data
 
 db_file_name = "Sanitized/club.db"
-query_file = "query1a.sql"
+query = """
+SELECT People.personID, first, last, Stati.text, Stati.key
+    FROM People, Person_Status, Stati
+    WHERE Person_Status.statusID = (SELECT statusID  
+        FROM Stati WHERE key = 'aw'
+    AND Person_Status.personID = People.personID)
+    ;
+-- The above query prints many lines, not just the one wanted!
+"""
+query = """
+SELECT People.personID, first, last, Stati.text, Stati.key
+    FROM People, Person_Status, Stati
+    WHERE Person_Status.statusID = Stati.statusID
+    AND Person_Status.personID = People.personID
+    AND Stati.key = 'aw'
+    ;
+"""
+
+
+def execute(cursor, connection, command):
+    try:
+        cursor.execute(command)
+    except (sqlite3.IntegrityError, sqlite3.OperationalError):
+        print("Unable to execute following query:")
+        print(command)
+        raise
+    connection.commit()
 
 
 def main():
     con = sqlite3.connect(db_file_name)
     cur = con.cursor()
-    for command in add_data.get_commands(query_file):
-        print(command)
-        print()
-        add_data.execute(cur, con, command)
-#       print(cur.fetchall())
-        for sequence in cur.fetchall():
-            print(sequence)
-            if sequence[-1] == 'aw':
-                print('\t{}'.format(sequence))
+    print(query)
+    print()
+    execute(cur, con, query)
+#   print(cur.fetchall())
+    for sequence in cur.fetchall():
+        print(sequence)
+        if sequence[-1] == 'aw':
+            print('\t{}'.format(sequence))
 
 
 
--- query2.py
+++ query2.py
@@ -3,24 +3,43 @@
 # File: query2.py
 
 import sqlite3
-import add_data
 
 db_file_name = "Sanitized/club.db"
-query_file = "query2.sql"
+
+q2 = """
+SELECT People.personID, first, last, Stati.text, Stati.key
+    FROM People
+    INNER JOIN Person_Status
+        ON People.personID = Person_Status.personID
+    INNER JOIN Stati
+        ON Person_Status.personID = Stati.statusID
+        AND Stati.key = 'aw'
+    ;
+-- this query gives the wrong answer!! No idea why.
+"""
+
+
+def execute(cursor, connection, command):
+    try:
+        cursor.execute(command)
+    except (sqlite3.IntegrityError, sqlite3.OperationalError):
+        print("Unable to execute following query:")
+        print(command)
+        raise
+    connection.commit()
 
 
 def main():
     con = sqlite3.connect(db_file_name)
     cur = con.cursor()
-    for command in add_data.get_commands(query_file):
-        print(command)
-        print()
-        add_data.execute(cur, con, command)
-#       print(cur.fetchall())
-        for sequence in cur.fetchall():
-            print(sequence)
-            if sequence[-1] == 'aw':
-                print(f'\t{sequence}')
+    print(q2)
+    print()
+    execute(cur, con, q2)
+#   print(cur.fetchall())
+    for sequence in cur.fetchall():
+        print(sequence)
+        if sequence[-1] == 'aw':
+            print(f'\t{sequence}')
 
 
 
