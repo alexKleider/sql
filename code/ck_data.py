b5e287feec42b08f2c981cf4e765bc10bd88987a
Alex Kleider <alexkleider@protonmail.com>
2024-02-16 15:48:29 -0800

    Delete lots of unused code.

--- code/ck_data.py
+++ code/ck_data.py
@@ -219,8 +219,10 @@ def gather_contacts_data(filter_func=None):
                 ret['names_by_group'][key].add(name_key)
     return ret
 
+# stati2include possibilities:
 applicants_and_members = (3,4,5,6,7,8,9,10,11,14,15,16,17)
 
+# restriction possibilities:
 not_email_restriction = " AND NOT email = '' "
 
 def gather_member_data(stati2include=None,
@@ -228,10 +230,10 @@ def gather_member_data(stati2include=None,
     """
     Collects data from the sql data base returning
     three dicts (similar to <gather_contacts_data()>)
-    <stati2include> (an iterable of strings) if provided
-    can limit what's returned.
-    <include_email> if set to False will exclude those without
-    an email entry although an empty string is still provided.
+    <stati2include> (an iterable of integers/statusIDs) if
+    provided can limit what's returned.
+    <restrictions> can be an optional clause to restrict
+    what's returned by the query. 
     """
     query_on_line = ''
     if stati2include:
@@ -269,243 +271,6 @@ def gather_member_data(stati2include=None,
         pass
     return ret
 
-
-def get_dict(source_file, sep=":", maxsplit=1):
-    """
-    # used by gather_extra_fees_data #
-    A generic function to parse files.
-    Blank lines or comments ('#') are ignored.
-    All other lines must contains a 'first last' name followed by
-    a separator (<sep> defaults to ':') and then anything else.
-    Returned is a dict keyed by 'last,first' name and value: the
-    string to right of <sep> (stripped of leading &/or trailing
-    spaces. (It could be an empty string!)
-    # Applicant data is populated one line at a time so this
-    # function is not useful there
-    """
-    ret = {}
-    with open(source_file, 'r') as stream:
-        for line in stream:
-            line = line.strip()
-            if not line or line[0] == '#': continue
-            parts = line.split(sep=sep, maxsplit=maxsplit)
-            if len(parts) != 2:
-                assert False, "Error in code/ck_data."
-            names = parts[0].split()
-            try:
-                name_key = '{},{}'.format(names[1], names[0])
-            except IndexError:
-                _ = input("IndexError re line: '{}'"
-                        .format(line))
-            ret[name_key] = parts[1].strip()
-    return ret
-
-
-def ck_data():
-    """
-    Check integrity/consistency of of the Club's data bases:
-    1.  MEMBERSHIP_SPoT  # the main club data base
-    2.  CONTACTS_SPoT    # csv downloaded from gmail
-    3.  APPLICANT_SPoT   #
-    4.  SPONSORS_SPoT    #
-    5.  EXTRA_FEES_SPoTs #
-        ...
-    The first 4 of the above all contain applicant data
-    and must be checked for consistency.  Data in the 2nd
-    and 5th must be consistent with that in the 1st.
-    
-    Returns a report (an array of lines) which (if <fee_details>
-    is set to True) can be extended to include any discrepencies
-    between what's billed each year vs what is still owed:
-    useful after payments begin to come in.
-
-    Consistency checks required:
-    -memlist-    names emails stati&fees  which_fee&amt
-    -contacts-   names emails  labels
-    -sponsors-   names (all sponsors are members?) 
-    -applicants- names        stati
-    -extra_fees- names                    which_fee&amt
-    [1] in future may keep records of non (no longer) members (and
-    expired applicants.)
-    """
-#   print("Entering data.ck_data")
-    holder.ret = []
-    holder.ok = []
-    holder.varying_amounts = []
-    holder.not_matching_notice = ''
-    helpers.add_header2list("Report Regarding Data Integrity",
-                holder.ret, underline_char='#', extra_line=True)
-#   gather_membership_data(holder)  # from main data base
-    gmail_data = gather_contacts_data()
-#   gather_extra_fees_data(holder)  # data comes from SPoTs
-#   populate_sponsor_data(holder)
-#   populate_applicant_data(holder)
-#   holder.applicants_by_status = get_applicants_by_status(holder)
-
-
-    ## First check that google groups match club data:
-    # Deal with extra fees...
-#   ck_malformed(holder)
-#   ck_fee_paying_labels(holder)  # google groups vs club data
-#   ck_fees_spots(holder)  # mem list vs extra fees SPoT
-    # Keep in mind that after payment amounts won't match
-    # Can use '-d' options for details.
-
-#   ck_gmail(holder)
-
-
-    ## do we compare gmail vs memlist emails anywhere????
-    ## None of the following are populated!!!
-    email_problems = []
-    missing_emails = []
-    non_member_contacts = []
-
-    redact4now = '''
-    if non_member_contacts:
-        helpers.add_sub_list(
-            "Contacts without a corresponding Member email",
-            non_member_contacts, holder.ret)
-    else:
-        holder.ok.append('No contacts that are not members.')
-    pass
-    emails_missing_from_contacts = []
-    common_emails = []
-
-    if emails_missing_from_contacts:
-        helpers.add_sub_list("Emails Missing from Google Contacts",
-                         emails_missing_from_contacts, holder.ret)
-    else:
-        holder.ok.append("No emails missing from gmail contacts.")
-'''
-
-#   ck_applicants(holder)
-
-    if holder.ok:
-        helpers.add_sub_list(
-            "No Problems with the Following", holder.ok, holder.ret)
-    ai_notice = "Acceptable Inconsistency"
-    if holder.not_matching_notice:
-        helpers.add_header2list(ai_notice,
-                                holder.ret, underline_char='=')
-        holder.ret.append(holder.not_matching_notice)
-    if holder.varying_amounts:
-        helpers.add_header2list(
-            "Fee Disparities: probably some have paid",
-            holder.ret, underline_char='-', extra_line=True)
-        holder.ret.extend(holder.varying_amounts)
-    return holder.ret
-
-
-
-
-def data_listed(data, underline_char='=', inline=False):
-    """
-    Assumes 'data' is a dict with list values.
-    Returns a list of lines: each key as a header +/- underlining
-    followed by its values one per line, or (if 'inline'=True) on
-    the same line separated by commas after a colon.
-    """
-    ret = []
-    keys = sorted(data.keys())
-    for key in keys:
-        values = sorted(data[key])
-        if inline:
-            ret.append(key + " :" + ", ".join(values))
-        else:
-            ret.append("\n" + key)
-            ret.append(underline_char * len(key))
-            ret.extend(values)
-    return ret
-
-
-def applicant_set():
-    """
-    Returns set of lsst/first names of current applicants.
-    """
-    query = """SELECT P.first, P.last, P.suffix
-            FROM people as P
-            JOIN Applicants as A
-            WHERE A.personID = P.personID
-                AND A.notified = '';
-    """
-    applicants = []
-    keys = "first, last, suffix".split(', ')
-    res = routines.fetch(query, from_file=False)
-    for item in res:
-        d = helpers.make_dict(keys, item)
-        if d['suffix']:
-            d['last'] = d['last'] + '_' + d['suffix'].strip()
-        applicants.append(f"{d['last']},{d['first']}")
-    app_set = set(applicants)
-    assert(len(app_set) == len(applicants)), 'Error in ck_data.applicant_set'
-    return app_set
-    for entry in applicants:
-        print(entry)
-
-
-def ck_labels():
-    """
-    """
-    report = []
-    keys = "first, last, suffix".split(', ')
-    contacts_data = gather_contacts_data()
-    labels = [key for key in queries.keys()]
-#   print(f"labels: {labels}")
-    for label in labels:
-        report.append(f"Dealing with: {label}")
-        category_lst = []
-        res = routines.fetch(queries[label],from_file=False)
-        for item in res:
-            d = helpers.make_dict(keys, item)
-            if d['suffix']:
-                d['last'] = (d['last'] + '_' +
-                        d['suffix'].strip())
-            category_lst.append(f"{d['last']},{d['first']}")
-        category_set = set(category_lst)
-        if len(category_set) != len(category_lst):
-            category_lst.sort()
-            duplicates = []
-            for i in range(1, len(category_lst)):
-                if category_lst[i] == category_lst[i-1]:
-                    duplicates.append(category_lst[i])
-            if duplicates:
-                report.append(f"Duplicates in {label}: {duplicates}")
-            redact = '''
-            report.append(f"""
-Error condition regarding {label}:
-    set:  {sorted(category_set)}
-    should be the same length as
-    list: {sorted(category_lst)}
-                """)
-            '''
-            report.append(f"len(category_set): {len(category_set)}")
-            report.append(f"len(category_lst): {len(category_lst)}")
-        try:
-            # gather_contacts_data returns a
-            # names_by_group dict => set
-            names_by_group = holder.names_by_group[label]
-        except IndexError:
-            report.extend([
-                f"There are no google label '{label}' entries",
-                f"to compare to {repr(category_set)}.",
-                ])
-        else:
-            if category_set != names_by_group:
-                _ = input(f"""The following do _not_ match:
-                    db {label}: {sorted(category_set)}
-                and
-                google {label}: {sorted(holder.names_by_group[label])}
-                """)
-                dif = (category_set -
-                        holder.names_by_group[label])
-                _ = input(f"{sorted(dif)}")
-                dif = (holder.names_by_group[label] -
-                        category_set)
-                _ = input(f"{sorted(dif)}")
-            else:
-                report.append(f"Labels match stati for {label}")
-
-    return report
 
 def mooring_dock():
     """
@@ -568,12 +333,13 @@ def ck_m_vs_g_data():
         report.append("...OK")
     return report
 
-def consistency_report(report):
+def consistency_report(report=None):
     """
     Called by menu.py under Reports/check_data_consistency
     """
+    if not report:
+        report = []
     report.extend(ck_m_vs_g_data())
-    report.extend(ck_labels())
     report.extend(mooring_dock())
     return report
 
@@ -602,10 +368,10 @@ def ck_stati_vs_labels():
 
 
 if __name__ == '__main__':
-#   for line in consistency_report([]): 
-#       print(line)
-    for line in ck_m_vs_g_data():
+    for line in consistency_report([]): 
         print(line)
+#   for line in ck_m_vs_g_data():
+#       print(line)
 #   ck_gather_contacts_data()
 #   res = gather_member_data()
     
